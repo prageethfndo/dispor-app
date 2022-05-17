def99a105452aec741a5b96403bd961d0de0d3c1
Venusha <venusha125@gmail.com>
2022-05-17 23:31:05 +0530

    change

--- App.js
+++ App.js
@@ -14,6 +14,7 @@ const App: () => Node = () => {
   //set the global accent color for all components
   const accentColor = '#038a37';
 
+  //change
   return <SellerMode accentColor={accentColor} />;
 };
 
